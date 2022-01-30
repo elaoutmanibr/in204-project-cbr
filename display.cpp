629821b1173257e2517073b8af7ab272730c0d49
brahim <brahimelaoutmani@gmail.com>
2022-01-30 12:29:42 +0100

    reorganisation

--- display.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  display.cpp
-//  ComicBookReader
-//
-//  Created by Youssef Ben Cheikh on 20/1/2022.
-//  Copyright © 2022 Youssef Ben Cheikh. All rights reserved.
-//
-
-#include "display.hpp"
--- imgprocess.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  imgprocess.cpp
-//  ComicBookReader
-//
-//  Created by Youssef Ben Cheikh on 28/1/2022.
-//  Copyright © 2022 Youssef Ben Cheikh. All rights reserved.
-//
-
-#include "imgprocess.hpp"
